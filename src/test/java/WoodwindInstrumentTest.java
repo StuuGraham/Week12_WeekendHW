e9e025cb1a15ff76f1f001899e997992c7c4d8e4
StuuGraham <stuartgraham7@hotmail.co.uk>
2021-01-17 18:17:14 +0000

    WoodwindInstrumentTest complete

--- src/test/java/WoodwindInstrumentTest.java
+++ src/test/java/WoodwindInstrumentTest.java
@@ -1,2 +1,24 @@
+import instruments.InstrumentType;
+import instruments.WoodwindInstrument;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WoodwindInstrumentTest {
+
+    WoodwindInstrument woodwindInstrument;
+
+    @Before
+    public void setUp(){
+        woodwindInstrument = new WoodwindInstrument("Oboe", "Silver", "Deedle Deeee",
+                79.99, 109.99, InstrumentType.WOODWIND, "Metal");
+    }
+
+    @Test
+    public void canCheckMaterialMadeOf(){
+        assertEquals("Metal", woodwindInstrument.getMaterialMadeOf());
+    }
+
+    // Not testing for other properties as they have already been shown as successful in StringInstrumentTest.
 }
